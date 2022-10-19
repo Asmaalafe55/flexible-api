4a65465b21a7bc5846867d032a066d8d2eb1a485
Ramie Bashir <ramiebash@gmail.com>
2022-10-19 19:31:39 +0300

    created sendMail function

--- utils/Nodemailer.js
+++ utils/Nodemailer.js
@@ -9,6 +9,41 @@ const REFRESH_TOKEN ='1//044HYLP--mv6OCgYIARAAGAQSNwF-L9IrB7XWgIq-4JgyImcUCWhMqA
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
 
+async function sendMail() {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken()
+
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: 'flexible.test1@gmail.com',
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        })
+
+        const mailOptions = {
+            from: 'flexibleEmailTest <flexible.test1@gmail.com>',
+            to: 'ramiebash@gmail.com',
+            subject: "Hello from gmail using API",
+            text: 'Hello from gmail email using API',
+            html: '<h1>Hello from gmail email using API</h1>'
+        }
+
+        const result = await transport.sendMail(mailOptions)
+        return result
+        
+    } catch (error) {
+        return error
+    }
+}
+
+sendMail().then(result => console.log('Email sent...', result))
+.catch(error => console.log(error.message))
+
  // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
