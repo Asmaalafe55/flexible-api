61b36ce2d531636c4282e2752924f5f4fa52e410
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-24 17:25:19 +0300

    removing coments

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,17 +50,3 @@ module.exports = {
   signin,
   signup,
 };
-
-/**
-- Add route for signup
-- Add route for signin
-- Add 'auth.js' controller 
-- In controller add signin and signup function (with catchSync like the nodemailer controller)
-- have a users.js model
-- Signup check for the data if it's wrong send back a response with status 400 
-   if correct hash the password with bcrypt and use a model function to insert the user into the database then sends response
-- Signin checks if data is given if yes use a model function to select the user (by username or email) and then compare the passwords
-
-if do not compare sends response with status 401 if correct create a token with jwt (the jwt payload would contain the email or username)
-and send the token in the response
-*/
