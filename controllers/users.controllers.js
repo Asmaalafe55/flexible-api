60750ccc5580d7a0a4505ca1f9136f57ecbe08f9
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-24 18:18:18 +0300

    changes updated

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,8 +1,9 @@
 const userModel = require('../models/users.model');
 const ApiError = require('../utils/ApiError');
 const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
 
-const getUserById = async (req, res) => {
+const getUserById = catchAsync(async (req, res) => {
   const id = req.body.id;
   if (!id) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Missing data');
@@ -12,9 +13,9 @@ const getUserById = async (req, res) => {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Data does not exist');
   }
   res.status(httpStatus.OK).send(result);
-};
+});
 
-const getUserByEmail = async (req, res) => {
+const getUserByEmail = catchAsync(async (req, res) => {
   const email = req.body.email;
   if (!email) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Missing data');
@@ -24,14 +25,11 @@ const getUserByEmail = async (req, res) => {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Data does not exist');
   }
   res.status(httpStatus.OK).send(result);
-};
+});
 
-const updateUsersDetails = async (req, res) => {
-  const first_name = req.body.first_name;
-  const last_name = req.body.last_name;
-  const email = req.body.email;
-  const password = req.body.password;
-  //should i check first/last name and password????
+const updateUsersDetails = catchAsync(async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+
   if (!email || !first_name || !last_name || !password) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Missing data');
   }
@@ -41,8 +39,11 @@ const updateUsersDetails = async (req, res) => {
     email,
     password
   );
+  if (!result) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Data does not exist');
+  }
   res.status(httpStatus.OK).send(result);
-};
+});
 
 module.exports = {
   getUserById,
