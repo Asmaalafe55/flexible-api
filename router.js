46c7bdb33e102c381ba01099a0e43b90d169ee6b
George Joubran <33234123+Iz1cK@users.noreply.github.com>
2022-10-24 20:18:57 +0300

    Update router.js

--- router.js
+++ router.js
@@ -1,13 +1,11 @@
 const hello = require('./controllers/index');
 const contactUs = require('./controllers/nodemailer');
-const contactUs = require('./controllers/nodemailer');
-const signin = require('./controllers/auth');
-const signup = require('./controllers/auth');
+const auth = require('./controllers/auth');
 
 const router = require('express').Router();
 
-router.get('/sign-in', signin);
-router.get('/sign-up', signup);
+router.post('/sign-in', auth.signin);
+router.post('/sign-up', auth.signup);
 
 router.get('/hello', hello);
 router.post('/contact', contactUs);
