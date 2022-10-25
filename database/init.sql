43817c06ec0c7b394ba8345d8f5bd3d04b7c85cf
“Mario <mario.saliba98@gmail.com>
2022-10-25 21:22:22 +0300

    fixes the questions data

--- database/init.sql
+++ database/init.sql
@@ -24,14 +24,14 @@ INSERT INTO users (user_id, first_name, last_name, email, password, register_dat
 
 INSERT INTO questions (question_type, question, answer_options, website_type) VALUES
 ('text', 'Event name', NULL, 'event'),
-('radio', 'How can people attend your event?', "[{value:"buy-tickets",text:"Buytickets",},{value:"signup-sheets",text:"Signupsheets",},{value:"just-show-up",text:"Justshowup"},{value:"contact-us",text:"Contactus",},]" , 'event'),
+('radio', 'How can people attend your event?', '[{"value":"buy-tickets","text":"Buytickets"},{"value":"signup-sheets","text":"Signupsheets"},{"value":"just-show-up","text":"Justshowup"},{"value":"contact-us","text":"Contactus"}]' , 'event'),
 ('text', 'What is the event address? (Fattoush, Haifa Auditorium, Zoom...)', NULL, 'event'),
 ('date dropdown', 'When is the event? (DD/MM/YYYY)', NULL, 'event'),
-('checkbox', 'What features would you like to add?', "[{value:"gallery",answerid:1,text:"Gallery"},{value:"socialmedialinks",answerid:2,text:"Socialmedialinks",},{value:"textboxes(blogstyle)",answerid:5,text:"Textboxes(blogstyle)",},{value:"countdown",answerid:6,text:"Countdown"},]", 'event'),
+('checkbox', 'What features would you like to add?', '[{"value":"gallery","text":"Gallery"},{"value":"socialmedialinks","text":"Socialmedialinks"},{"value":"textboxes(blogstyle)","text":"Textboxes(blogstyle)"},{"value":"countdown","text":"Countdown"}]', 'event'),
 ('text', 'Name on blog', NULL, 'blog'),
-('checkbox', 'What features would you like to add?', "[{value:"gallery",text:"Gallery"},{value:"socialmedialinks",text:"Socialmedialinks",},{value:"blog",text:"Blog"},{value:"chat",text:"Chat"}]", 'blog'),
-('checkbox', 'How would you like your readers to contact you?', "[{value:"email",text:"Email"},{value:"phone",text:"Phone",},{value:"text",text:"Text"}]", 'blog'),
-('radio', 'Would you like to add pagination or infinite scrolling for your posts?', "[{value:"yes",text:"Yes",},{value:"no",text:"No",},]", 'blog')
+('checkbox', 'What features would you like to add?', '[{"value":"gallery","text":"Gallery"},{"value":"socialmedialinks","text":"Socialmedialinks"},{"value":"blog","text":"Blog"},{"value":"chat","text":"Chat"}]', 'blog'),
+('checkbox', 'How would you like your readers to contact you?', '[{"value":"email","text":"Email"},{"value":"phone","text":"Phone"},{"value":"text","text":"Text"}]', 'blog'),
+('radio', 'Would you like to add pagination or infinite scrolling for your posts?', '[{"value":"yes","text":"Yes"},{"value":"no","text":"No"}]', 'blog');
 
 
 COMMIT;
