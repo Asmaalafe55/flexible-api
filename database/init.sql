fb27c8456f058e40645fc3ffe2e14b735d5a2307
Ramie Bashir <ramiebash@gmail.com>
2022-10-25 15:11:38 +0300

    inserted the last 3 event questions into the questions table

--- database/init.sql
+++ database/init.sql
@@ -12,17 +12,22 @@ CREATE TABLE users (
 );
 
 CREATE TABLE questions (
-  question_id INTEGER PRIMARY KEY,
+  question_id SERIAL PRIMARY KEY,
   question_type VARCHAR(20) NOT NULL,
   question VARCHAR(150) NOT NULL,
-  answers JSON,
+  answer_options JSON,
   website_type VARCHAR(20) NOT NULL
 );
 
 INSERT INTO users (user_id, first_name, last_name, email, password, register_date) VALUES
 (2340,'First Name','Last Name','helloworld@gmail.com','helloworld','2022-10-17');
 
-INSERT INTO questions (question_id, question_type, question, answers) VALUES
-(2347, 3,'Question','["Berlin", "London"]');
+INSERT INTO questions (question_type, question, answer_options, website_type) VALUES
+('textbox', 'Event name', NULL, 'event'),
+('radio', 'How can people attend your event?', '["Buy tickets", "Sign-up sheet (free)", "Just show up", "Contact us"]', 'event'),
+('textbox', 'What is the event address?', NULL, 'event'),
+('date dropdown', 'When is the event?', NULL, 'event'),
+('checkboxes', 'What features would you like to add?', '["Gallery", "Social media links", "Music", "Videos", "Text boxes (blog style)", "Countdown"]', 'event'),
+
 
 COMMIT;
