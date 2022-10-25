3d39b06043cfe702b70de0c5f80413cc4f262aa9
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2022-10-25 20:48:16 +0300

    Update init.sql
    Updated questions + answer options

--- database/init.sql
+++ database/init.sql
@@ -23,15 +23,15 @@ INSERT INTO users (user_id, first_name, last_name, email, password, register_dat
 (2340,'First Name','Last Name','helloworld@gmail.com','helloworld','2022-10-17');
 
 INSERT INTO questions (question_type, question, answer_options, website_type) VALUES
-('textbox', 'Event name', NULL, 'event'),
-('radio', 'How can people attend your event?', '["Buy tickets", "Sign-up sheet (free)", "Just show up", "Contact us"]', 'event'),
-('textbox', 'What is the event address?', NULL, 'event'),
-('date dropdown', 'When is the event?', NULL, 'event'),
-('checkboxes', 'What features would you like to add?', '["Gallery", "Social media links", "Music", "Videos", "Text boxes (blog style)", "Countdown"]', 'event'),
+('text', 'Event name', NULL, 'event'),
+('radio', 'How can people attend your event?', "[{value:"buy-tickets",text:"Buytickets",},{value:"signup-sheets",text:"Signupsheets",},{value:"just-show-up",text:"Justshowup"},{value:"contact-us",text:"Contactus",},]" , 'event'),
+('text', 'What is the event address? (Fattoush, Haifa Auditorium, Zoom...)', NULL, 'event'),
+('date dropdown', 'When is the event? (DD/MM/YYYY)', NULL, 'event'),
+('checkbox', 'What features would you like to add?', "[{value:"gallery",answerid:1,text:"Gallery"},{value:"socialmedialinks",answerid:2,text:"Socialmedialinks",},{value:"textboxes(blogstyle)",answerid:5,text:"Textboxes(blogstyle)",},{value:"countdown",answerid:6,text:"Countdown"},]", 'event'),
 ('text', 'Name on blog', NULL, 'blog'),
-('checkboxes', 'What features would you like to add?', '["Gallery", "Search", "Social Media Links", "Commenting system"]', 'blog'),
-('checkboxes', 'How would you like your readers to contact you?', '["Email", "Phone", "Text"]', 'blog'),
-('radio', 'Would you like to add pagination or infinite scrolling for your posts?', '["Yes", "No"]', 'blog')
+('checkbox', 'What features would you like to add?', "[{value:"gallery",text:"Gallery"},{value:"socialmedialinks",text:"Socialmedialinks",},{value:"blog",text:"Blog"},{value:"chat",text:"Chat"}]", 'blog'),
+('checkbox', 'How would you like your readers to contact you?', "[{value:"email",text:"Email"},{value:"phone",text:"Phone",},{value:"text",text:"Text"}]", 'blog'),
+('radio', 'Would you like to add pagination or infinite scrolling for your posts?', "[{value:"yes",text:"Yes",},{value:"no",text:"No",},]", 'blog')
 
 
 COMMIT;
